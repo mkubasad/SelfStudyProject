d9e58aba6543b082a2f199907808a16f9a4fbe24
manju <mkubasad100@gmail.com>
2018-04-17 10:49:03 +0530

    Added printlns to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i1=4, i2;
+		int i1=4, i2=1;
 		System.out.println("Hello World!");
 		
 		System.out.print("2X2 = ");
 		System.out.println(i1);
+		System.out.println(" ");
+		System.out.println(i1*i2);
 
 	}
 
