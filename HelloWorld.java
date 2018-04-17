c311dcd3490527b31669e009fcb3e61e149c6e16
manju <mkubasad100@gmail.com>
2018-04-17 10:41:37 +0530

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i1=4, i2;
+		System.out.println("Hello World!");
+		
+		System.out.print("2X2 = ");
+		System.out.println(i1);
+
+	}
+
+}
